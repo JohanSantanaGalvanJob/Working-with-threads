91ecb5ced56524981ed1ae01656885db3df5bdb5
Johan <JohanSantanaGalvan.Job@gmail.com>
2022-05-20 12:36:50 +0100

    third commit

--- src/sancochocherne/Hilo.java
+++ src/sancochocherne/Hilo.java
@@ -4,15 +4,13 @@ import java.util.concurrent.Semaphore;
 
 public class Hilo extends Thread {
 
-    public Semaphore sem1,sem2,sem3,sem4,sem5,sem6,sem7,sem8,sem9,sem10,sem11,sem12,sem13;
+    public Semaphore sem1,sem2,sem3,sem4,sem5,sem6,sem7;
 
     public Hilo(String texto, Semaphore se1,Semaphore se2,Semaphore se3,Semaphore se4,Semaphore
-se5,Semaphore se6,Semaphore se7,Semaphore se8,Semaphore se9,Semaphore se10,Semaphore
-se11,Semaphore se12,Semaphore se13) {
+se5,Semaphore se6,Semaphore se7) {
         super(texto);
         this.sem1=se1; this.sem2=se2; this.sem3=se3; this.sem4=se4; this.sem5=se5; this.sem6=se6;
-        this.sem7=se7; this.sem8=se8; this.sem9=se9; this.sem10=se10; this.sem11=se11; this.sem12=se12;
-        this.sem13=se13;
+        this.sem7=se7;;
         
     }
 
@@ -34,95 +32,78 @@ se11,Semaphore se12,Semaphore se13) {
         
         if (this.getName().equals("H3")) {
             System.out.println("H3 Calentar aceite a medio fuego");
-            sem3.release(); //desbloqueo semáforo 3.
+            sem2.release(); //desbloqueo semáforo 3.
         } 
         
         if (this.getName().equals("H4")) {
             try{
                 sem2.acquire(); //espero por el semáforo 2 
             } catch (InterruptedException e){}
-            try{
-                sem3.acquire(); //espero por el semáforo 3 
-            } catch (InterruptedException e){}
             System.out.println("H4 Pochar las papas");
-            sem4.release(); //desbloqueo semáforo 4.
+            sem3.release(); //desbloqueo semáforo 4.
         } 
         
         if (this.getName().equals("H5")) {
             System.out.println("H5 Trocear tomate por la mitad");
-            sem5.release(); //desbloqueo semáforo 5.
+            sem3.release(); //desbloqueo semáforo 5.
         } 
         
         if (this.getName().equals("H6")) {
             System.out.println("H6 Trocear pimiento");
-            sem6.release(); //desbloqueo semáforo 6.
+            sem3.release(); //desbloqueo semáforo 6.
         } 
         
         if (this.getName().equals("H7")) {
             System.out.println("H7 Poner sal ajo y perejil al cherne antes de colocarlo en la fuente de horno");
-            sem7.release(); //desbloqueo semáforo 7.
+            sem3.release(); //desbloqueo semáforo 7.
         } 
         
         if (this.getName().equals("H8")) {
             try{
-                sem4.acquire(); //espero por el semáforo 4 
+                sem3.acquire(); //espero por el semáforo 4 
             } catch (InterruptedException e){}
-            try{
-                sem5.acquire(); //espero por el semáforo 5 
-            } catch (InterruptedException e){}
-            try{
-                sem6.acquire(); //espero por el semáforo 6 
-            } catch (InterruptedException e){}
-            try{
-                sem7.acquire(); //espero por el semáforo 7 
-            } catch (InterruptedException e){}
+           
             System.out.println("H8 Colocar en una fuente de horno las papas pochadas con el tomate, el pimiento y el Cherne");
-            sem8.release(); //desbloqueo semáforo 8.
+            sem4.release(); //desbloqueo semáforo 8.
         } 
         
         if (this.getName().equals("H9")) {
             System.out.println("H9 Precalentar horno a 270 grados centígrados");
-            sem9.release(); //desbloqueo semáforo 9.
+            sem4.release(); //desbloqueo semáforo 9.
         } 
         
         if (this.getName().equals("H10")) {
             try{
-                sem8.acquire(); //espero por el semáforo 8 
-            } catch (InterruptedException e){}
-            try{
-                sem9.acquire(); //espero por el semáforo 9 
+                sem4.acquire(); //espero por el semáforo 8 
             } catch (InterruptedException e){}
             System.out.println("H10 Meter fuente con todo en el horno precalentado a 270º solo 5 minutos para que se marque");
-            sem10.release(); //desbloqueo semáforo 10.
+            sem5.release(); //desbloqueo semáforo 10.
         } 
         
         if (this.getName().equals("H11")) {
             System.out.println("H11 Calentar vaso de vino blanco hasta llevarlo a ebullición");
-            sem11.release(); //desbloqueo semáforo 11.
+            sem5.release(); //desbloqueo semáforo 11.
         } 
         
         if (this.getName().equals("H12")) {
             try{
-                sem10.acquire(); //espero por el semáforo 10 
-            } catch (InterruptedException e){}
-            try{
-                sem11.acquire(); //espero por el semáforo 11 
+                sem5.acquire(); //espero por el semáforo 10 
             } catch (InterruptedException e){}
             System.out.println("H12 Tras marcar el pescado, rocía el vaso de vino blanco por todo el pescado");
-            sem12.release(); //desbloqueo semáforo 12.
+            sem6.release(); //desbloqueo semáforo 12.
         } 
         
         if (this.getName().equals("H13")) {
             try{
-                sem12.acquire(); //espero por el semáforo 12 
+                sem6.acquire(); //espero por el semáforo 12 
             } catch (InterruptedException e){}
             System.out.println("H13 Poner el horno a 160 grados y cocinar durante 20 minutos más");
-            sem13.release(); //desbloqueo semáforo 13.
+            sem7.release(); //desbloqueo semáforo 13.
         } 
         
         if (this.getName().equals("H14")) {
             try{
-                sem13.acquire(); //espero por el semáforo 13 
+                sem7.acquire(); //espero por el semáforo 13 
             } catch (InterruptedException e){}
             System.out.println("H14 Servir caliente");
         } 
--- src/sancochocherne/SancochoCherne.java
+++ src/sancochocherne/SancochoCherne.java
@@ -7,46 +7,40 @@ public class SancochoCherne {
 
     public static void main(String[] args) {
         Semaphore se1 = new Semaphore(0);
-        Semaphore se2 = new Semaphore(0);
-        Semaphore se3 = new Semaphore(0);
-        Semaphore se4 = new Semaphore(0);
-        Semaphore se5 = new Semaphore(0);
+        Semaphore se2 = new Semaphore(-1);
+        Semaphore se3 = new Semaphore(-3);
+        Semaphore se4 = new Semaphore(-1);
+        Semaphore se5 = new Semaphore(-1);
         Semaphore se6 = new Semaphore(0);
         Semaphore se7 = new Semaphore(0);
-        Semaphore se8 = new Semaphore(0);
-        Semaphore se9 = new Semaphore(0);
-        Semaphore se10 = new Semaphore(0);
-        Semaphore se11 = new Semaphore(0);
-        Semaphore se12 = new Semaphore(0);
-        Semaphore se13 = new Semaphore(0);
 
-        Hilo H1 = new Hilo("H1", se1, se2, se3, se4, se5, se6,se7, se8, se9, se10, se11, se12,se13);
+        Hilo H1 = new Hilo("H1", se1, se2, se3, se4, se5, se6,se7);
         H1.start();
-        Hilo H2 = new Hilo("H2", se1, se2, se3, se4, se5, se6,se7, se8, se9, se10, se11, se12,se13);
+        Hilo H2 = new Hilo("H2", se1, se2, se3, se4, se5, se6,se7);
         H2.start();
-        Hilo H3 = new Hilo("H3", se1, se2, se3, se4, se5, se6,se7, se8, se9, se10, se11, se12,se13);
+        Hilo H3 = new Hilo("H3", se1, se2, se3, se4, se5, se6,se7);
         H3.start();
-        Hilo H4 = new Hilo("H4", se1, se2, se3, se4, se5, se6,se7, se8, se9, se10, se11, se12,se13);
+        Hilo H4 = new Hilo("H4", se1, se2, se3, se4, se5, se6,se7);
         H4.start();
-        Hilo H5 = new Hilo("H5", se1, se2, se3, se4, se5, se6,se7, se8, se9, se10, se11, se12,se13);
+        Hilo H5 = new Hilo("H5", se1, se2, se3, se4, se5, se6,se7);
         H5.start();
-        Hilo H6 = new Hilo("H6", se1, se2, se3, se4, se5, se6,se7, se8, se9, se10, se11, se12,se13);
+        Hilo H6 = new Hilo("H6", se1, se2, se3, se4, se5, se6,se7);
         H6.start();
-        Hilo H7 = new Hilo("H7", se1, se2, se3, se4, se5, se6,se7, se8, se9, se10, se11, se12,se13);
+        Hilo H7 = new Hilo("H7", se1, se2, se3, se4, se5, se6,se7);
         H7.start();
-         Hilo H8 = new Hilo("H8", se1, se2, se3, se4, se5, se6,se7, se8, se9, se10, se11, se12,se13);
+         Hilo H8 = new Hilo("H8", se1, se2, se3, se4, se5, se6,se7);
         H8.start();
-        Hilo H9 = new Hilo("H9", se1, se2, se3, se4, se5, se6,se7, se8, se9, se10, se11, se12,se13);
+        Hilo H9 = new Hilo("H9", se1, se2, se3, se4, se5, se6,se7);
         H9.start();
-        Hilo H10 = new Hilo("H10", se1, se2, se3, se4, se5, se6,se7, se8, se9, se10, se11, se12,se13);
+        Hilo H10 = new Hilo("H10", se1, se2, se3, se4, se5, se6,se7);
         H10.start();
-        Hilo H11 = new Hilo("H11", se1, se2, se3, se4, se5, se6,se7, se8, se9, se10, se11, se12,se13);
+        Hilo H11 = new Hilo("H11", se1, se2, se3, se4, se5, se6,se7);
         H11.start();
-        Hilo H12 = new Hilo("H12", se1, se2, se3, se4, se5, se6,se7, se8, se9, se10, se11, se12,se13);
+        Hilo H12 = new Hilo("H12", se1, se2, se3, se4, se5, se6,se7);
         H12.start();
-        Hilo H13 = new Hilo("H13", se1, se2, se3, se4, se5, se6,se7, se8, se9, se10, se11, se12,se13);
+        Hilo H13 = new Hilo("H13", se1, se2, se3, se4, se5, se6,se7);
         H13.start();
-        Hilo H14 = new Hilo("H14", se1, se2, se3, se4, se5, se6,se7, se8, se9, se10, se11, se12,se13);
+        Hilo H14 = new Hilo("H14",se1, se2, se3, se4, se5, se6,se7);
         H14.start();
 
     }
